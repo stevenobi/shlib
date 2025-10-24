a62ebacf043412fc74d7959d32ead56816be0ff6
stevenobi <s.obermeyer@t-online.de>
2025-10-24 10:27:57 +0200

    Add copyright notice to shlib.sh
    Signed-off-by: stevenobi <s.obermeyer@t-online.de>

--- shlib.sh
+++ shlib.sh
@@ -22,6 +22,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Copyright: 2025 sobermeyer
+#
 ################################################################################
 set +x
 ################################################################################
